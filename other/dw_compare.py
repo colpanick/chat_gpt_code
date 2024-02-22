f08feae87b93b32c407667f0fbaeb35c6b5dc60a
George Miles <gmiles83@gmail.com>
2024-02-22 14:17:43 -0500

    dw_compare glob added

--- other/dw_compare.py
+++ other/dw_compare.py
@@ -1,8 +1,9 @@
 import csv
+import glob
 
-def compare_csv_files(file1, file2, column_number, output_columns, output_file):
-    data1 = read_csv(file1)
-    data2 = read_csv(file2)
+def compare_csv_files(file1_pattern, file2_pattern, column_number, output_columns, output_file):
+    data1 = read_csv_files(file1_pattern)
+    data2 = read_csv_files(file2_pattern)
 
     column_values1 = set(row[column_number] for row in data1)
     column_values2 = set(row[column_number] for row in data2)
@@ -14,6 +15,12 @@ def compare_csv_files(file1, file2, column_number, output_columns, output_file):
     unique_rows += [row for row in data2 if row[column_number] in unique_values_in_file2]
 
     write_csv(unique_rows, output_columns, output_file)
+
+def read_csv_files(file_pattern):
+    data = []
+    for file_path in glob.glob(file_pattern):
+        data += read_csv(file_path)
+    return data
 
 def read_csv(file_path):
     with open(file_path, 'r') as file:
@@ -27,10 +34,10 @@ def write_csv(data, columns, output_file):
         writer.writerows([row[col] for col in columns] for row in data)
 
 if __name__ == "__main__":
-    file1_path = "file1.csv"
-    file2_path = "file2.csv"
+    file1_pattern = "files1/*.csv"
+    file2_pattern = "files2/*.csv"
     column_number = 1  # Change to the desired column number (0-based index)
     output_columns = ["Column1", "Column2", "Column3"]  # Change to the desired column names
     output_file_path = "output_file.csv"
 
-    compare_csv_files(file1_path, file2_path, column_number, output_columns, output_file_path)
+    compare_csv_files(file1_pattern, file2_pattern, column_number, output_columns, output_file_path)
